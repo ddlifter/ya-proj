fc984c8ebc55bd8f16d8f4d849dfba062798872c
ddlifter <butorin_kirya@inbox.ru>
2024-03-08 14:36:26 +0300

    added yml file for CI

---
+++ internal/services/agent/calculate/calculate_test.go
@@ -0,0 +1,22 @@
+package calculate
+
+import "testing"
+
+func TestEvaluateExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected float64
+	}{
+		{"2+2", 4},
+		{"(2+2)*3", 12},
+		{"(4*2)/2", 4},
+		{"(1+2)*(2-1)-(1+1)", 1},
+	}
+
+	for _, test := range tests {
+		result := EvaluateExpression(test.input)
+		if result != test.expected {
+			t.Errorf("For expression %s, expected %f but got %f", test.input, test.expected, result)
+		}
+	}
+}
